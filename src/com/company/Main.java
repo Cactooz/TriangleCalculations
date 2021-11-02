a314111721825261e39449099a1b52887a908acd
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-11-02 11:50:13 +0100

    Initial Commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,41 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        Triangle triangle;
+
+        System.out.println("Välj ett val:");
+        System.out.println("1. Höjd och bredd");
+        System.out.println("2. Tre sidors längd");
+
+        byte answer = input.nextByte();
+
+        if (answer == 1) {
+            System.out.print("Triangelns höjd: ");
+            float height = input.nextFloat();
+
+            System.out.print("Triangelns bredd: ");
+            float width = input.nextFloat();
+
+            triangle = new Triangle(width, height);
+        }
+        else if (answer == 2) {
+            float[] sides = new float[2];
+
+            System.out.print("Triangelns sidor: ");
+            for (int i = 0; i < 3; i++) {
+                System.out.print("Sida " + i + ": ");
+                sides[i] = input.nextFloat();
+            }
+            float height = input.nextFloat();
+            triangle = new Triangle(sides);
+        }
+
+
+    }
+}
