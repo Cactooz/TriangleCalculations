ef965956f4d86952cea7807839f5237e123dbedc
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-11-15 11:31:25 +0100

    Split side array into its own variables

--- src/com/company/Triangle.java
+++ src/com/company/Triangle.java
@@ -1,7 +1,7 @@
 package com.company;
 
 public class Triangle {
-	private float width, height;
+	private float width, height, side1, side2, side3;
 	private float[] sides;
 	
 	public Triangle(float triangleWidth, float triangleHeight) {
@@ -17,6 +17,12 @@ public class Triangle {
 			System.out.println("Omkrets: " + CircumferenceWithSides());
 			System.out.println("Area: " + AreaWithSides());
 		}
+	}
+	
+	private void splitArray() {
+		side1 = sides[0];
+		side2 = sides[1];
+		side3 = sides[2];
 	}
 	
 	private float CircumferenceWithSides() {
