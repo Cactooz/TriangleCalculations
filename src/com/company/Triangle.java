d07f7294886ff39c99a6475618f96753150afaf9
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-11-15 10:22:45 +0100

    Tab indentation

--- src/com/company/Triangle.java
+++ src/com/company/Triangle.java
@@ -1,24 +1,21 @@
 package com.company;
 
 public class Triangle {
-
-    private float width, height;
-    private float[] sides;
-
-    public Triangle(float triangleWidth, float triangleHeight) {
-        width = triangleWidth;
-        height = triangleHeight;
-    }
-    public Triangle(float[] triangleSides) {
-        sides = triangleSides;
-    }
-
-    public void Calculate() {
-
-    }
-
-
-
-    //Check if empty
-    //width.isEmpty();
+	private float width, height;
+	private float[] sides;
+	
+	public Triangle(float triangleWidth, float triangleHeight) {
+		width = triangleWidth;
+		height = triangleHeight;
+	}
+	public Triangle(float[] triangleSides) {
+		sides = triangleSides;
+	}
+	
+	public void Calculate() {
+	
+	}
+	
+	//Check if empty
+	//width.isEmpty();
 }
