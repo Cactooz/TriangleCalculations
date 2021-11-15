c20535dfdb9dff3b1e59e4dd35b8b80afb5671f7
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-11-15 10:23:49 +0100

    Tab indentation

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,37 +3,37 @@ package com.company;
 import java.util.Scanner;
 
 public class Main {
-
-    public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
-
-        Triangle triangle;
-
-        System.out.println("Välj ett val:");
-        System.out.println("1. Höjd och bredd");
-        System.out.println("2. Tre sidors längd");
-
-        byte answer = input.nextByte();
-
-        if (answer == 1) {
-            System.out.print("Triangelns höjd: ");
-            float height = input.nextFloat();
-
-            System.out.print("Triangelns bredd: ");
-            float width = input.nextFloat();
-
-            triangle = new Triangle(width, height);
-        }
-        else if (answer == 2) {
-            float[] sides = new float[3];
-
-            System.out.println("Triangelns sidor: ");
-            for (int i = 0; i < 3; i++) {
-                System.out.print("Sida " + (i+1) + ": ");
-                sides[i] = input.nextFloat();
-            }
-            triangle = new Triangle(sides);
-        }
-
-    }
+	
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		
+		Triangle triangle;
+		
+		System.out.println("Välj ett val:");
+		System.out.println("1. Höjd och bredd");
+		System.out.println("2. Tre sidors längd");
+		
+		byte answer = input.nextByte();
+		
+		if (answer == 1) {
+			System.out.print("Triangelns höjd: ");
+			float height = input.nextFloat();
+			
+			System.out.print("Triangelns bredd: ");
+			float width = input.nextFloat();
+			
+			triangle = new Triangle(width, height);
+		}
+		else if (answer == 2) {
+			float[] sides = new float[3];
+			
+			System.out.println("Triangelns sidor: ");
+			for (int i = 0; i < 3; i++) {
+				System.out.print("Sida " + (i+1) + ": ");
+				sides[i] = input.nextFloat();
+			}
+			triangle = new Triangle(sides);
+		}
+		
+	}
 }
