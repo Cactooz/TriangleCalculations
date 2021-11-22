5be9890b48a072d7b70e3bbb4808420481c2e981
Cactooz <54435874+Cactooz@users.noreply.github.com>
2021-11-22 08:51:28 +0100

    Converted to static methods

--- src/com/company/Triangle.java
+++ src/com/company/Triangle.java
@@ -27,19 +27,19 @@ public class Triangle {
 	}
 	
 	//Split the array with the 3 sides into variables that can be used in the other methods
-	private void splitArray() {
+	public static void splitArray() {
 		side1 = sides[0];
 		side2 = sides[1];
 		side3 = sides[2];
 	}
 	
 	//Get the circumference of the triangle
-	private float Circumference(float s1, float s2, float s3) {
+	public static float Circumference(float s1, float s2, float s3) {
 		return s1 + s2 + s3;
 	}
 	
 	//Get the area of a triangle that we only know the 3 sides of
-	private float AreaWithSides(float s1, float s2, float s3) {
+	public static float AreaWithSides(float s1, float s2, float s3) {
 		float sp = Semiperimeter(s1, s2, s3);
 		
 		double area = Math.sqrt(sp*(sp-s1)*(sp-s2)*(sp-s3));
@@ -48,17 +48,17 @@ public class Triangle {
 	}
 	
 	//Get the median point of the triangle
-	private float Median(float middle, float a, float b) {
+	public static float Median(float middle, float a, float b) {
 		double median = Math.sqrt((2*a*a)+(2*b*b)-(middle*middle));
 		
 		return (float) median/2;
 	}
 	
-	private float Semiperimeter(float s1, float s2, float s3) {
+	public static float Semiperimeter(float s1, float s2, float s3) {
 		return Circumference(s1, s2, s3) / 2;
 	}
 	
-	private float CircleInTriangle(float s1, float s2, float s3) {
+	public static float CircleInTriangle(float s1, float s2, float s3) {
 		float sp = Semiperimeter(s1, s2, s3);
 		
 		double circleRadius = Math.sqrt(((sp-s1)*(sp-s2)*(sp-s3))/sp);
@@ -67,13 +67,13 @@ public class Triangle {
 	}
 	
 	//Get the third side of the triangle using pythagoras formula
-	private float getThirdSide(float s1, float s2) {
+	public static float getThirdSide(float s1, float s2) {
 		double s3 = Math.sqrt((s1*s1)+(s2*s2));
 		
 		return (float) s3;
 	}
 	
-	private float Bisector(float s1, float s2, float alpha) {
+	public static float Bisector(float s1, float s2, float alpha) {
 		double p = 2 * s1 * s2 * Math.cos (alpha / 2);
 		double bisector = p / (s1 + s2);
 		return (float) bisector;
